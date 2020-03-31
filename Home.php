e6fdbc02555cca50da9e857ef13dcc53a8ca9bac
Gabriele Virbasiute <g.virbasiute@gmail.com>
2020-03-31 14:00:22 +0100

    Linked to the new navbar.php

--- Home.php
+++ Home.php
@@ -13,7 +13,7 @@
         <link href="CarouselCSS.css" rel="stylesheet" type="text/css"/>
     </head>
     <body>
-        <?php include 'navbar.html'; ?>
+        <?php include 'navbar.php'; ?>
     <center><h1 style="font-family: 'Amatic SC', cursive; font-size: 100px"> Happy crafting!</h1></center>
     <h2 style=" font-family: 'Amatic SC', cursive; font-size: 50px; font-weight: bold; text-align: center;">Our top tutorials:</h2>
     <?php include 'Carousel.html'; ?>
