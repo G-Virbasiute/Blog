d98a99acf60b0c11145c2abee057288a81042eb4
Gabriele Virbasiute <g.virbasiute@gmail.com>
2020-04-02 11:50:13 +0100

    Renamed a few more things and put some of them into folders.

--- CategoryPage.php
+++ CategoryPage.php
@@ -12,7 +12,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-        <link href="stylesheets/blogpost.css" rel="stylesheet" type="text/css"/>        
+        <link href="stylesheets/post.css" rel="stylesheet" type="text/css"/>        
         <link href="https://fonts.googleapis.com/css?family=Amatic+SC&display=swap" rel="stylesheet">
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
@@ -33,10 +33,10 @@
                             <tr>
                                 <?php
                                 echo '<div>';
-                                echo '<h1><a href="blogpost.php?PostID=' . $post['PostID'] . '">' . $post['Title'] . '</a></h1>';
+                                echo '<h1><a href="post.php?PostID=' . $post['PostID'] . '">' . $post['Title'] . '</a></h1>';
                                 echo '<p>Posted on ' . ($post['Published']) . '</p>';
                                 echo '<p>' . $post['Blurb'] . '</p>';
-                                echo '<p><a href="blogpost.php?PostID=' . $post['PostID'] . '">Read More</a></p>';
+                                echo '<p><a href="post.php?PostID=' . $post['PostID'] . '">Read More</a></p>';
                                 echo '</div>';
                                 ?>
                             </tr>
